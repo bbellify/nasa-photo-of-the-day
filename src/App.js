cb0865adb56aafd43617457cabe7d688c058ff83
Brian Bellinger <brianpatrickbellinger@gmail.com>
2021-10-06 12:42:59 -0700

    constants added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from "react";
 import "./App.css";
+import { BASE_URL, API_KEY } from './constants'
 
 function App() {
   return (
     <div className="App">
       <p>
         Read through the instructions in the README.md file to build your NASA
-        app! Have fun <span role="img" aria-label='go!'>🚀</span>!
+        app! Have hella fun <span role="img" aria-label='go!'>🚀</span>!
       </p>
+      {console.log(`${BASE_URL}`)}
     </div>
   );
 }
---
+++ src/constants/index.js
@@ -0,0 +1,2 @@
+export const BASE_URL = 'https://api.nasa.gov/planetary/apod'
+export const API_KEY = 'dQ3pk2fQvFpmlb25bVIE1igqV4NBWN0gFoqidwpy'
