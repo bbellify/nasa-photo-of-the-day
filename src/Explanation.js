3d631a657c4c7cdede9ce0b5bf8ff06e746eba4d
Brian Bellinger <brianpatrickbellinger@gmail.com>
2021-10-06 16:38:15 -0700

    added button to explanation

--- src/Explanation.js
+++ src/Explanation.js
@@ -1,9 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 
 export default function Explanation(props) {
     const { explanation } = props;
+
+    const [exState, setExState] = useState(false)
+
     return (
-        <p>{explanation}</p>
+        <div>
+            <button onClick={() => setExState(!exState)}>{exState ? 'Hide Explanation' : 'Show Explanation'}</button>
+            {exState && <p>{explanation}</p>}
+        </div>
+        
     )
 }
--- src/Title.js
+++ src/Title.js
@@ -1,9 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 
 export default function Title(props){
     const { title } = props;
     return (
-        <h2 className='title'>{title}</h2>
-    )
+        <h2>{title}</h2>
+    )        
 }
